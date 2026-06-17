b3fa21ca97e979dc545a18ec304056d62ce011bd
Lolbit <mzidanwibowo20@gmail.com>
2026-06-17 16:37:30 +0700

    Refactor student ID check to support multiple students

--- LogicIndex.js
+++ LogicIndex.js
@@ -2,21 +2,58 @@ const button = document.querySelector(".btn");
 
 const result = document.getElementById("result");
 
-const student = {
+const students = [
 
-  id: "20112010",
-  name: "Kai",
-  saldo: 50000
+  {
+    id: "20112010",
+    name: "Kai",
+    saldo: 50000
+  },
 
-};
+  {
+    id: "20112011",
+    name: "Ahmad",
+    saldo: 75000
+  },
 
+  {
+    id: "20112012",
+    name: "Zidan",
+    saldo: 100000
+  }
+
+];
+
+// Cari student berdasarkan ID
+function findStudent(id){
+
+  return students.find(
+    student => student.id === id
+  );
+
+}
+
+// Tombol Checking
 button.onclick = function(){
 
   const input =
   document.querySelector("input").value;
 
-  if(input === student.id){
-    
+  const student =
+  findStudent(input);
+
+  if(student){
+
+    localStorage.setItem(
+      "studentName",
+      student.name
+    );
+
+    localStorage.setItem(
+      "studentSaldo",
+      student.saldo
+    );
+
     window.location.href =
     "dashboard.html";
 
@@ -30,3 +67,20 @@ button.onclick = function(){
   }
 
 };
+
+// Tekan Enter = klik tombol
+const inputField =
+document.querySelector("input");
+
+inputField.addEventListener(
+  "keydown",
+  function(event){
+
+    if(event.key === "Enter"){
+
+      button.click();
+
+    }
+
+  }
+);
